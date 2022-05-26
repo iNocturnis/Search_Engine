a567424a5425c2ccdd366e8cc8685bee76ea8e51
unknown <anguyen1772@gmail.com>
2022-05-25 18:41:36 -0700

    created new tf-idf and changed posting class

--- posting.py
+++ posting.py
@@ -1,10 +1,9 @@
 #Posting class for indexer, will probably be more complex as we keep adding crap to it
 
 class Posting():
-	def __init__(self,url,tf_idf):
+	def __init__(self, url, rtf, position):
 		self.url = url
-		self.tf_idf = tf_idf
-		
-	def comparator(self):
-		#Some custom comparator for sorting postings later
-		pass
+		self.rtf = rtf
+		self.tf = 0
+		self.tfidf = 0
+		self.positions = [position]
